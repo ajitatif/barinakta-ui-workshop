ab33135ebb790d9b306052d9bfc461aab12abecf
ajitatif <gokalp.gurbuzer@yandex.com>
2017-01-10 02:15:00 +0300

    some template-ing

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'barinakta-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/ui/app.html'
 })
 export class AppComponent  { name = 'Angular'; }
