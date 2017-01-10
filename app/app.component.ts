58d3b5453801ebacbcce09e740e32e520c2d1e29
ajitatif <gokalp.gurbuzer@yandex.com>
2017-01-10 21:47:11 +0300

    template-ing complete

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,4 +4,8 @@ import { Component } from '@angular/core';
   selector: 'barinakta-app',
   templateUrl: 'app/ui/app.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+
+    shelters = [ { 'name': 'Kadıköy', 'phone': '0 (216) 415 67 11' }, { 'name': 'Üsküdar', 'phone': '0 (216) 393 00 10' } ];
+
+}
